3fdbe54551a7fe545b8b9f3dc6425fc38222a095
Desepto <chwebaka@hotmail.fr>
2015-11-13 11:01:13 +0100

    Ajout de l'exo 2

---
+++ azerty/src/test/java/fr/uvsq/coo/ex3_2/azerty/TestGame.java
@@ -0,0 +1,66 @@
+package fr.uvsq.coo.ex3_2.azerty;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestGame {
+
+	private Game g;
+
+	@Before
+	public void setUp() throws Exception {
+		g = new Game();
+	}
+
+	@Test
+	public void testGutterGame() throws Exception {
+		rollMany(20, 0);
+		assertEquals(0, g.score());
+	}
+
+	private void rollMany(int n, int pins) {
+		for (int i = 0; i < n; i++)
+			g.roll(pins);
+	}
+
+	@Test
+	public void testAllOnes() throws Exception {
+		rollMany(20, 1);
+		assertEquals(20, g.score());
+	}
+
+	@Test
+	public void testOneSpare() throws Exception {
+		rollSpare();
+		g.roll(3);
+		rollMany(17, 0);
+		assertEquals(16, g.score());
+	}
+
+	private void rollSpare() {
+		g.roll(5);
+		g.roll(5);
+	}
+
+	@Test
+	public void testOneStrike() throws Exception {
+		rollStrike();
+		g.roll(3);
+		g.roll(4);
+		rollMany(16, 0);
+		assertEquals(24, g.score());
+	}
+
+	private void rollStrike() {
+		g.roll(10);
+	}
+
+	@Test
+	public void testPerfectGame() throws Exception {
+		rollMany(12, 10);
+		assertEquals(300, g.score());
+	}
+
+}
